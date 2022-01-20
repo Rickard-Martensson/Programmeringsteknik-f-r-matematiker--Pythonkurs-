4ea8d23202e4d3bb21086dbb0c8546481544b31f
Ricke <rickard.isak.martensson@gmail.com>
2022-01-20 11:06:29 +0100

    added comments to konvertera.py

--- laborationer/konvertera.py
+++ laborationer/konvertera.py
@@ -1,12 +1,41 @@
-def fahrenheit_to_celsius(f):
+def fahrenheit_to_celsius(f: int):
+    """
+    Converts a temperature in farenheit to a temperature in celcius
+
+    Args:
+        f (int): [a temperature in farenheit]
+
+    Returns:
+        [float]: [a temperateture in celcius]
+
+    Example usage:
+    122 -> 50.0
+    -40 -> -40.0
+    """
     return (f - 32) * (5 / 9)
 
 
-def celsius_to_farenheit(c):
+def celsius_to_farenheit(c: int):
+    """
+    Converts a temperature in celcius to a temperature in farenheit
+
+    Args:
+        f (int): [a temperature in celcius]
+
+    Returns:
+        [float]: [a temperateture in farenheit]
+
+    Example usage:
+    50 -> 122.0
+    -40 -> -40.0
+    """
     return c * (9 / 5) + 32
 
 
 def to_islam():
+    """
+    Forces the user to recite the Shahada, converting them to the one true faith
+    """
     trosbekännelse = "Det finns ingen gud utom Gud och Muhammed är hans sändebud"
     while True:
         print("säg efter mig:", trosbekännelse)
